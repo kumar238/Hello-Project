085946dd5e44052697d454c300384cdaf0c5a311
Srikanth13Reddy <srikanthy@apptomate.co>
2021-11-17 09:09:13 +0530

    abcd

--- Hello Project/Startup.cs
+++ Hello Project/Startup.cs
@@ -32,7 +32,7 @@ namespace Hello_Project
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Welcome To C# World!");
                 });
             });
         }
